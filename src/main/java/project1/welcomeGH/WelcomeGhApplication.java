cc6293d7359265d474a6083efb75d632dde95730
sooin92402 <sooin92402@gmail.com>
2022-01-17 16:43:50 +0900

    first commit

---
+++ src/main/java/project1/welcomeGH/WelcomeGhApplication.java
@@ -0,0 +1,13 @@
+package project1.welcomeGH;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WelcomeGhApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WelcomeGhApplication.class, args);
+	}
+
+}
---
+++ src/test/java/project1/welcomeGH/WelcomeGhApplicationTests.java
@@ -0,0 +1,13 @@
+package project1.welcomeGH;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WelcomeGhApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
